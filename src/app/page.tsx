5f76d0ab8b1bc8a47abc409eaa51706e67636859
Damilola Oduronbi <doduronbi@gmail.com>
2024-11-28 19:15:16 +0000

    chore: install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Terminal } from "lucide-react"
+
 export default function Home() {
   return (
-    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Welcome to <strong>doduronbi.com</strong></h1>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div>
+        <h1>
+          <Terminal className="h-4 w-4" /> Welcome to{" "}
+          <strong>doduronbi.com</strong>
+        </h1>
+      </div>
     </div>
   );
 }
