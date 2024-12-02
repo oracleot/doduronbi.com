ad996f3682f405af3fcbcbbdb19dbb8ec915285f
Damilola Oduronbi <doduronbi@gmail.com>
2024-12-02 13:33:44 +0000

    Improve my intro

--- src/components/composites/IntroBanner.tsx
+++ src/components/composites/IntroBanner.tsx
@@ -13,12 +13,9 @@ export default function IntroBanner() {
         <h1 className="text-3xl md:text-5xl font-orbitron font-medium">
           Damilola Oduronbi
         </h1>
-        <div className="flex">
-          <h2 className="text-lg md:text-xl font-semibold">Full Stack JavaScipt Developer</h2>
-        </div>
+        <h2 className="text-lg md:text-xl">Software Engineer | Exploring AI & LLMs</h2>
         <h3 className="px-4 md:px-0 text-center text-[var(--mid-gray)] max-w-[500px]">
-          Crafting scalable solutions and driving digital innovation with React,
-          Node.js, and Azure for over 6 years.
+        <strong>Building scalable, high-performance solutions</strong> with a broad tech stack including modern web frameworks, languages, and cloud platforms to help businesses innovate and thrive.
         </h3>
       </div>
     </div>
