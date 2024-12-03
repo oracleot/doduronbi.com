19e2ccec43e710ed86d09d540e43a6f9ec8e5c60
Damilola Oduronbi <doduronbi@gmail.com>
2024-12-03 20:26:43 +0000

    Add clients section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import IntroBanner from "@/components/composites/IntroBanner";
-import Link from "next/link";
+import IntroBanner from '@/components/composites/IntroBanner';
+import Link from 'next/link';
 
-import { Button } from "@/components/ui/button";
-import { FileText, Github, Linkedin, Mail, Slash } from "lucide-react";
-import CopyButton from "@/components/composites/CopyButton";
-import { Fragment } from "react";
+import { Button } from '@/components/ui/button';
+import { FileText, Github, Linkedin, Mail, Slash } from 'lucide-react';
+import CopyButton from '@/components/composites/CopyButton';
+import { Fragment } from 'react';
+import FramerCarouselComponent from '@/components/composites/FramerCarousel';
 
-const LINKEDIN_URL = "https://linkedin.com/in/doduronbi";
-const GITHUB_URL = "https://github.com/oracleot";
-const RESUME_URL = "https://docs.google.com/document/d/11x_aKqKuolZj3bCXA-_XicOJwGEV8d3d2NvB3nzw8XQ";
+const LINKEDIN_URL = 'https://linkedin.com/in/doduronbi';
+const GITHUB_URL = 'https://github.com/oracleot';
+const RESUME_URL =
+  'https://docs.google.com/document/d/11x_aKqKuolZj3bCXA-_XicOJwGEV8d3d2NvB3nzw8XQ';
 
 const portfolioSites: { title: string; url: string }[] = [
-  { title: "LinkedIn", url: LINKEDIN_URL },
-  { title: "Github", url: GITHUB_URL },
-  { title: "Resume", url: RESUME_URL },
+  { title: 'LinkedIn', url: LINKEDIN_URL },
+  { title: 'Github', url: GITHUB_URL },
+  { title: 'Resume', url: RESUME_URL },
 ];
 
 export default function Home() {
@@ -21,27 +23,29 @@ export default function Home() {
     <>
       <Header />
       <IntroBanner />
+      {/* <CarouselComponent /> */}
+      <FramerCarouselComponent />
     </>
   );
 }
 
 function Header() {
   return (
-    <header className="bg-[var(--light-gray)] pt-8 px-3">
-      <div className="container hidden md:flex justify-between items-center mx-auto h-[3rem] flex-wrap">
-        <section className="flex gap-3">
-          <CopyButton text="doduronbi@gmail.com" />
-          <Button asChild className="rounded-full" size="lg">
-            <Link href="mailto:doduronbi@gmail.com" target="_blank">
+    <header className='bg-[var(--light-gray)] pt-8 px-3'>
+      <div className='container hidden md:flex justify-between items-center mx-auto h-[3rem] flex-wrap'>
+        <section className='flex gap-3'>
+          <CopyButton text='doduronbi@gmail.com' />
+          <Button asChild className='rounded-full' size='lg'>
+            <Link href='mailto:doduronbi@gmail.com' target='_blank'>
               <Mail /> Send Email
             </Link>
           </Button>
         </section>
-        <section className="flex items-center">
+        <section className='flex items-center'>
           {portfolioSites.map((item, index) => (
             <Fragment key={index}>
-              <Button asChild variant="link">
-                <Link href={item.url} target="_blank">
+              <Button asChild variant='link'>
+                <Link href={item.url} target='_blank'>
                   {item.title}
                 </Link>
               </Button>
@@ -50,27 +54,39 @@ function Header() {
           ))}
         </section>
       </div>
-      <div className="container flex md:hidden justify-between items-center mx-auto h-[3rem] flex-wrap">
-        <section className="flex gap-3">
-          <Button asChild className="rounded-full" size="lg">
-            <Link href="mailto:doduronbi@gmail.com" target="_blank">
+      <div className='container flex md:hidden justify-between items-center mx-auto h-[3rem] flex-wrap'>
+        <section className='flex gap-3'>
+          <Button asChild className='rounded-full' size='lg'>
+            <Link href='mailto:doduronbi@gmail.com' target='_blank'>
               <Mail /> Send Email
             </Link>
           </Button>
         </section>
-        <section className="flex gap-3">
-          <Button asChild variant="outline" size="icon" className="rounded-full">
-            <Link href={LINKEDIN_URL} target="_blank">
+        <section className='flex gap-3'>
+          <Button
+            asChild
+            variant='outline'
+            size='icon'
+            className='rounded-full'>
+            <Link href={LINKEDIN_URL} target='_blank'>
               <Linkedin />
             </Link>
           </Button>
-          <Button asChild variant="outline" size="icon" className="rounded-full">
-            <Link href={GITHUB_URL} target="_blank">
+          <Button
+            asChild
+            variant='outline'
+            size='icon'
+            className='rounded-full'>
+            <Link href={GITHUB_URL} target='_blank'>
               <Github />
             </Link>
           </Button>
-          <Button asChild variant="outline" size="icon" className="rounded-full">
-            <Link href={RESUME_URL} target="_blank">
+          <Button
+            asChild
+            variant='outline'
+            size='icon'
+            className='rounded-full'>
+            <Link href={RESUME_URL} target='_blank'>
               <FileText />
             </Link>
           </Button>
--- src/components/composites/IntroBanner.tsx
+++ src/components/composites/IntroBanner.tsx
@@ -1,21 +1,24 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function IntroBanner() {
   return (
     <div
-      className={`flex items-center justify-center bg-[var(--light-gray)] min-h-[30rem] lg:min-h-[40rem] rounded-b-[6rem]`}
-    >
-      <div className="flex items-center flex-col gap-3">
-        <Avatar className="w-[6rem] h-[6rem]">
-          <AvatarImage src="images/moi.jpeg" alt="@shadcn" />
-          <AvatarFallback className="text-[2rem]">DO</AvatarFallback>
+      className={`flex items-center justify-center bg-[var(--light-gray)] min-h-[30rem] lg:min-h-[40rem] rounded-b-[6rem]`}>
+      <div className='flex items-center flex-col gap-3'>
+        <Avatar className='w-[6rem] h-[6rem]'>
+          <AvatarImage src='images/moi.jpeg' alt='@shadcn' />
+          <AvatarFallback className='text-[2rem]'>DO</AvatarFallback>
         </Avatar>
-        <h1 className="text-3xl md:text-5xl font-orbitron font-medium">
+        <h1 className='text-3xl md:text-5xl font-orbitron font-medium'>
           Damilola Oduronbi
         </h1>
-        <h2 className="text-lg md:text-xl">Software Engineer | Exploring AI & LLMs</h2>
-        <h3 className="px-4 md:px-0 text-center text-[var(--mid-gray)] max-w-[500px]">
-        <strong>Building scalable, high-performance solutions</strong> with a broad tech stack including modern web frameworks, languages, and cloud platforms to help businesses innovate and thrive.
+        <h2 className='text-lg md:text-xl'>
+          Software Engineer | Exploring AI & LLMs
+        </h2>
+        <h3 className='px-4 md:px-0 text-center text-[var(--mid-gray)] max-w-[500px]'>
+          <strong>Building scalable, high-performance solutions</strong> with a
+          broad tech stack including modern web frameworks, languages, and cloud
+          platforms to help businesses innovate and thrive.
         </h3>
       </div>
     </div>
